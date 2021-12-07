adf16139451470b861c163a7f1b949e2a90c104c
weissleon <weissleon1943@gmail.com>
2021-12-07 19:43:35 +0900

    Add OverworldMap

--- app/classes/Overworld.js
+++ app/classes/Overworld.js
@@ -1,34 +1,31 @@
 import GameObject from './GameObject.js';
-
+import OverworldMap from './OverworldMap.js';
 class Overworld {
 	constructor(config) {
 		this.element = config.element;
 		this.canvas = this.element.querySelector('#game-canvas');
 		this.ctx = this.canvas.getContext('2d');
+		this.map;
+	}
+
+	startGameLoop() {
+		const step = () => {
+			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+			this.map.drawLowerImage(this.ctx);
+			Object.values(this.map.gameObjects).forEach((object) => {
+				// object.position.x += 0.01;
+				object.sprite.draw(this.ctx);
+			});
+			this.map.drawUpperImage(this.ctx);
+			requestAnimationFrame(step);
+		};
+		step();
 	}
 
 	init() {
-		const image = new Image();
-		image.src = './images/maps/DemoLower.png';
-		image.onload = () => {
-			this.ctx.drawImage(image, 0, 0);
-		};
-
-		// Placing Game Objects
-		const hero = new GameObject({
-			position: { x: 5, y: 6 },
-			src: '/images/characters/people/hero.png',
-		});
-
-		const npc1 = new GameObject({
-			position: { x: 7, y: 9 },
-			src: '/images/characters/people/npc1.png',
-		});
-
-		setTimeout(() => {
-			hero.sprite.draw(this.ctx);
-			npc1.sprite.draw(this.ctx);
-		}, 200);
+		this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+		this.startGameLoop();
 	}
 }
 
